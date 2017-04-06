b07c23dfb45794f08f066978917fad80e21ed1df
Lyubov Bormotova <jukaenerdjaizer@gmail.com>
2017-04-06 22:04:58 +0300

    adaptive header, banner, screen 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-	$(".mainMenu-button").on("click", function(e) {
-		e.preventDefault();
+	$(".mainMenu-button").on("click", function() {
 		$(".mainMenu").slideToggle(300);
 		$(this).toggleClass("active");
+
+		return false;
 	});
 });
